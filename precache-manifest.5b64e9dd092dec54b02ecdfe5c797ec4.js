d1eb1b9ada2c51771cac0260094ac10138289a8e
JeffDemanche <jdemanche@horselywitten.com>
2019-12-18 22:38:03 -0500

    Updates

---
+++ precache-manifest.5b64e9dd092dec54b02ecdfe5c797ec4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a130e856322122b8f5f8b5c7683d71",
+    "url": "/cs1300-portfolio/index.html"
+  },
+  {
+    "revision": "c4e3f344c8e0670bbf89",
+    "url": "/cs1300-portfolio/static/css/main.fa2341ae.chunk.css"
+  },
+  {
+    "revision": "a3a5512193b8c21a8a1a",
+    "url": "/cs1300-portfolio/static/js/2.f8a2924b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cs1300-portfolio/static/js/2.f8a2924b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4e3f344c8e0670bbf89",
+    "url": "/cs1300-portfolio/static/js/main.99c63b7e.chunk.js"
+  },
+  {
+    "revision": "0a1e399df1419df86cb6",
+    "url": "/cs1300-portfolio/static/js/runtime-main.af5e69a5.js"
+  },
+  {
+    "revision": "23fac9477a06b5a54e9d5dc4b8b3d69f",
+    "url": "/cs1300-portfolio/static/media/insta.23fac947.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/cs1300-portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "99891c969abffcdc62aad0cd30c194b1",
+    "url": "/cs1300-portfolio/static/media/me.99891c96.jpg"
+  },
+  {
+    "revision": "c2821b7578d185a9926fb5d04d349b2d",
+    "url": "/cs1300-portfolio/static/media/mylk-2.c2821b75.PNG"
+  },
+  {
+    "revision": "fba92f70d2ee2774baf8d81a68025b5a",
+    "url": "/cs1300-portfolio/static/media/mylk-3.fba92f70.PNG"
+  },
+  {
+    "revision": "7498ca0daadea9608ad991d59766e11e",
+    "url": "/cs1300-portfolio/static/media/mylk.7498ca0d.png"
+  },
+  {
+    "revision": "a8de7362f817657a8fbcb5b607357b11",
+    "url": "/cs1300-portfolio/static/media/post.a8de7362.png"
+  },
+  {
+    "revision": "a6a48464f56a6220f7a2671510e25bd9",
+    "url": "/cs1300-portfolio/static/media/prov-background.a6a48464.jpg"
+  },
+  {
+    "revision": "7ec6d8c94e2c8043eac5b3b07ed80e4f",
+    "url": "/cs1300-portfolio/static/media/tvfinder.7ec6d8c9.PNG"
+  },
+  {
+    "revision": "bdda731ebba4a90449e000fc3e80c999",
+    "url": "/cs1300-portfolio/static/media/twitter.bdda731e.png"
+  },
+  {
+    "revision": "2b6dc127597b9e69ccb6c9a18a17e60e",
+    "url": "/cs1300-portfolio/static/media/uiux-2.2b6dc127.PNG"
+  },
+  {
+    "revision": "5e4b2ec8a6821aa615f9d83e11a1720e",
+    "url": "/cs1300-portfolio/static/media/uiux.5e4b2ec8.PNG"
+  }
+]);
