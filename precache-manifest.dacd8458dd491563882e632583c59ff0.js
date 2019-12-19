f3859bf204b6867efcef9302dd1d7186f58ea8f5
JeffDemanche <jdemanche@horselywitten.com>
2019-12-18 22:36:39 -0500

    Updates

---
+++ precache-manifest.dacd8458dd491563882e632583c59ff0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164993f90b2da896b0cabd8f8a11ae52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "367fb1c242ef38ea3d7c",
+    "url": "/static/css/main.fa2341ae.chunk.css"
+  },
+  {
+    "revision": "a3a5512193b8c21a8a1a",
+    "url": "/static/js/2.f8a2924b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.f8a2924b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "367fb1c242ef38ea3d7c",
+    "url": "/static/js/main.c810bf7f.chunk.js"
+  },
+  {
+    "revision": "05937374f27229e3d99a",
+    "url": "/static/js/runtime-main.8dfd5373.js"
+  },
+  {
+    "revision": "23fac9477a06b5a54e9d5dc4b8b3d69f",
+    "url": "/static/media/insta.23fac947.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "99891c969abffcdc62aad0cd30c194b1",
+    "url": "/static/media/me.99891c96.jpg"
+  },
+  {
+    "revision": "c2821b7578d185a9926fb5d04d349b2d",
+    "url": "/static/media/mylk-2.c2821b75.PNG"
+  },
+  {
+    "revision": "fba92f70d2ee2774baf8d81a68025b5a",
+    "url": "/static/media/mylk-3.fba92f70.PNG"
+  },
+  {
+    "revision": "7498ca0daadea9608ad991d59766e11e",
+    "url": "/static/media/mylk.7498ca0d.png"
+  },
+  {
+    "revision": "a8de7362f817657a8fbcb5b607357b11",
+    "url": "/static/media/post.a8de7362.png"
+  },
+  {
+    "revision": "a6a48464f56a6220f7a2671510e25bd9",
+    "url": "/static/media/prov-background.a6a48464.jpg"
+  },
+  {
+    "revision": "7ec6d8c94e2c8043eac5b3b07ed80e4f",
+    "url": "/static/media/tvfinder.7ec6d8c9.PNG"
+  },
+  {
+    "revision": "bdda731ebba4a90449e000fc3e80c999",
+    "url": "/static/media/twitter.bdda731e.png"
+  },
+  {
+    "revision": "2b6dc127597b9e69ccb6c9a18a17e60e",
+    "url": "/static/media/uiux-2.2b6dc127.PNG"
+  },
+  {
+    "revision": "5e4b2ec8a6821aa615f9d83e11a1720e",
+    "url": "/static/media/uiux.5e4b2ec8.PNG"
+  }
+]);
